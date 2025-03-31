f25a347754d2a640b9c1f37d5a06487c2bf81d7b
paalrebnordbrugger <paalbrugger@gmail.com>
2025-03-31 19:09:22 +0200

    Fix layout

--- ClientApp/src/pages/AppLayout.jsx
+++ ClientApp/src/pages/AppLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import style from "./AppLayout.module.css";
+
 function AppLayout() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
   return (
     <div>
       <Header />
-      <main>
+      <main className={isHome ? "" : style["with-padding"]}>
         <Outlet />
       </main>
     </div>
