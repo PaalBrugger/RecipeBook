edfe656c085559eef6b7abb0a1852068afd8475b
paalrebnordbrugger <paalbrugger@gmail.com>
2025-03-31 19:08:54 +0200

    Fix RecipeCard

--- ClientApp/src/components/RecipeCard.jsx
+++ ClientApp/src/components/RecipeCard.jsx
@@ -6,18 +6,14 @@ function RecipeCard({ recipe }) {
     <div
       className={`card h-100 w-100 rounded-4 ${styles["card-background"]} ${styles["recipe-card"]}`}
     >
-      {recipe ? (
-        <div className="card-body">
-          <img
-            src={recipe.strMealThumb}
-            className="img-fluid rounded-4"
-            alt="Recipe"
-          ></img>
-          <h3 className="pt-3">{recipe.strMeal}</h3>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="card-body">
+        <img
+          src={recipe.strMealThumb}
+          className="img-fluid rounded-4"
+          alt="Recipe"
+        ></img>
+        <h3 className="pt-3">{recipe.strMeal}</h3>
+      </div>
     </div>
   );
 }
