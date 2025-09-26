1a157bf603438858388b20b95452f99d8a701687
paalrebnordbrugger <paalbrugger@gmail.com>
2025-09-26 15:45:49 +0200

    Removed toaster

--- ClientApp/src/components/ProtectedRoute.jsx
+++ ClientApp/src/components/ProtectedRoute.jsx
@@ -1,22 +1,10 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../services/AuthProvider";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
-  const [redirecting, setRedirecting] = useState(false);
 
-  //Show toast first, then redirect
-  useEffect(() => {
-    if (!isAuthenticated) {
-      toast.error("Unauthorized - please log in");
-      setRedirecting(true);
-    }
-  }, [isAuthenticated]);
-
-  // Redirect if not authenticated
-  if (!isAuthenticated && redirecting) {
+  if (!isAuthenticated) {
     return <Navigate to="/Login" replace />;
   }
 
