e7841f5ecfd188f76e6caecd452704b19d156dcd
paalrebnordbrugger <paalbrugger@gmail.com>
2025-12-04 15:35:07 +0100

    Added API endpoints

---
+++ RecipeBook.API/Models/RecipeDTO.cs
@@ -0,0 +1,16 @@
+namespace RecipeBook.API.Models;
+
+public class RecipeDTO
+{
+    public int Id { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    public string Name { get; set; }
+    public string? Area { get; set; }
+    public string? Category { get; set; }
+    public string? Instructions { get; set; }
+    public string? MainImageUrl { get; set; }
+    public string? Source  { get; set; }
+    public string? Youtube  { get; set; }
+    
+    public List<IngredientDTO> Ingredients { get; set; }
+}
