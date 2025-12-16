e1b1a64b1c702afecc35228d71ee9e746639aac3
paalrebnordbrugger <paalbrugger@gmail.com>
2025-12-16 14:51:25 +0100

    Added URL´s

--- ClientApp/src/utils/apiUrls.js
+++ ClientApp/src/utils/apiUrls.js
@@ -4,3 +4,6 @@ export const FILTER_URL = `${API_BASE_URL}/recipe/filter`;
 export const RANDOM_URL = `${API_BASE_URL}/recipe/random`;
 export const SEARCH_URL = `${API_BASE_URL}/recipe/search`;
 export const LOOKUP_ID_URL = `${API_BASE_URL}/recipe/`;
+export const FAVORITE_RECIPE_URL = `${API_BASE_URL}/recipe/favorite-recipe`;
+export const UNFAVORITE_RECIPE_URL = `${API_BASE_URL}/recipe/unfavorite-recipe`;
+export const ISFAVORITED_RECIPE_URL = `${API_BASE_URL}/recipe/is-favorited`;
