3f0a89fdf629150b90bccb2ec0ef499361390cf1
paalrebnordbrugger <paalbrugger@gmail.com>
2025-12-19 13:48:19 +0100

    Switched to new API

--- ClientApp/src/pages/Favorites.jsx
+++ ClientApp/src/pages/Favorites.jsx
@@ -1,38 +1,32 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import RecipeCard from "../components/RecipeCard";
-import Spinner from "../components/Spinner";
-import { LOOKUP_ID_URL } from "../utils/apiUrls";
+import { GET_FAVORITED_RECIPES } from "../utils/apiUrls";
+import { useAuth } from "../services/AuthProvider";
 import RecipeContainer from "../components/RecipeContainer";
 
 function Favorites() {
-  const [favorites, setFavorites] = useState([]);
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const stored = JSON.parse(localStorage.getItem("favorites") || "[]");
-    setFavorites(stored);
-  }, []);
+  const { token } = useAuth();
 
   useEffect(() => {
     async function fetchFavorites() {
-      if (favorites.length === 0) {
-        setRecipes([]);
-        return;
+      const res = await fetch(GET_FAVORITED_RECIPES, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch favorite status");
       }
-      const fetches = favorites.map((id) =>
-        fetch(LOOKUP_ID_URL + id).then((res) => res.json())
-      );
-
-      const results = await Promise.all(fetches);
-      setRecipes(results);
+      const data = await res.json();
+      setRecipes(data);
       setIsLoading(false);
     }
 
     fetchFavorites();
-  }, [favorites]);
+  }, []);
 
+  // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const recipesPerPage = 12;
   const totalPages = Math.ceil(recipes.length / recipesPerPage);
@@ -44,15 +38,13 @@ function Favorites() {
   return (
     <div className="py-2">
       <h2 className="mb-4">Your Favorite Recipes 🍽️</h2>
-      <div className="pt-4">
-        <RecipeContainer
-          recipes={paginatedRecipes}
-          isLoading={isLoading}
-          currentPage={currentPage}
-          totalPages={totalPages}
-          setCurrentPage={setCurrentPage}
-        />
-      </div>
+      <RecipeContainer
+        recipes={paginatedRecipes}
+        isLoading={isLoading}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
