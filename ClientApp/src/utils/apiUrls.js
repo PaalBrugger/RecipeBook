b2b00f0152cf610fcf4fbf79ce1f584e2870f7db
paalrebnordbrugger <paalbrugger@gmail.com>
2025-12-22 12:54:00 +0100

    Updated URL´s

--- ClientApp/src/utils/apiUrls.js
+++ ClientApp/src/utils/apiUrls.js
@@ -4,7 +4,10 @@ export const FILTER_URL = `${API_BASE_URL}/recipe/filter`;
 export const RANDOM_URL = `${API_BASE_URL}/recipe/random`;
 export const SEARCH_URL = `${API_BASE_URL}/recipe/search`;
 export const LOOKUP_ID_URL = `${API_BASE_URL}/recipe/`;
+
 export const FAVORITE_RECIPE_URL = `${API_BASE_URL}/recipe/favorite-recipe`;
 export const UNFAVORITE_RECIPE_URL = `${API_BASE_URL}/recipe/unfavorite-recipe`;
 export const ISFAVORITED_RECIPE_URL = `${API_BASE_URL}/recipe/is-favorited`;
-export const GET_FAVORITED_RECIPES = `${API_BASE_URL}/recipe/get-favorited-recipes`;
+export const GET_FAVORITED_RECIPES_URL = `${API_BASE_URL}/recipe/get-favorited-recipes`;
+
+export const USER_URL = "http://localhost:5091/api/user/me";
