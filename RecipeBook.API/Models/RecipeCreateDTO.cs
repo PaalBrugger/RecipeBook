ea0714afc59cddfa1576353bbabba626e76bfe4c
paalrebnordbrugger <paalbrugger@gmail.com>
2026-01-29 18:45:49 +0100

    Added RecipeCreateDTO.cs

---
+++ RecipeBook.API/Models/RecipeCreateDTO.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBook.API.Models;
+
+public class RecipeCreateDTO
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Category { get; set; }
+    [Required]
+    public string Area { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public string? Source { get; set; }
+    public string? Youtube { get; set; }
+
+    public string? Instructions { get; set; }
+
+    public IFormFile? ImageFile { get; set; }
+
+    public List<IngredientDTO> Ingredients { get; set; } = new();
+}
