f6a6f331d5b0b25c166908f29c029cd203341ddc
paalrebnordbrugger <paalbrugger@gmail.com>
2026-01-29 18:45:16 +0100

    Made fields nullable

--- RecipeBook.API/Models/IngredientDTO.cs
+++ RecipeBook.API/Models/IngredientDTO.cs
@@ -3,6 +3,6 @@ namespace RecipeBook.API.Models;
 public class IngredientDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Measure { get; set; }
+    public string? Name { get; set; }
+    public string? Measure { get; set; }
 }
