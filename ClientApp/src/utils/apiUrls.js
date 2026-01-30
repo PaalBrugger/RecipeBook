ee397ab4c722f0fa344c5c28572c5c1b61164c4a
paalrebnordbrugger <paalbrugger@gmail.com>
2026-01-30 17:46:11 +0100

    Added Delete URL

--- ClientApp/src/utils/apiUrls.js
+++ ClientApp/src/utils/apiUrls.js
@@ -1,10 +1,14 @@
+// Base
 export const BASE_URL = "http://localhost:5091";
 export const API_BASE_URL = "http://localhost:5091/api";
 
+// Recipe
 export const FILTER_URL = `${API_BASE_URL}/recipe/filter`;
 export const RANDOM_URL = `${API_BASE_URL}/recipe/random`;
 export const SEARCH_URL = `${API_BASE_URL}/recipe/search`;
 export const LOOKUP_ID_URL = `${API_BASE_URL}/recipe/`;
+
+// User
 
 export const FAVORITE_RECIPE_URL = `${API_BASE_URL}/user/favorite-recipe`;
 export const UNFAVORITE_RECIPE_URL = `${API_BASE_URL}/user/unfavorite-recipe`;
@@ -16,3 +20,4 @@ export const USER_URL = `${API_BASE_URL}/user/me`;
 export const CREATE_RECIPE_URL = `${API_BASE_URL}/user/create-recipe`;
 export const GET_USER_CREATED_RECIPES_URL = `${API_BASE_URL}/user/get-user-created-recipes`;
 export const UPDATE_RECIPE_URL = `${API_BASE_URL}/user/update-recipe`;
+export const DELETE_RECIPE_URL = `${API_BASE_URL}/user/delete-recipe`;
