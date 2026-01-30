96c43ec3059f0bd23d8313fe3d9278dd3300358d
paalrebnordbrugger <paalbrugger@gmail.com>
2026-01-30 15:20:11 +0100

    Added user recipes

--- ClientApp/src/pages/UserRecipes.jsx
+++ ClientApp/src/pages/UserRecipes.jsx
@@ -2,9 +2,42 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import RecipeContainer from "../components/RecipeContainer";
+import { authFetch } from "../utils/authFetch";
+import { GET_USER_CREATED_RECIPES_URL } from "../utils/apiUrls";
+import { useAuth } from "../services/AuthProvider";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 function UserRecipes() {
-  useEffect(() => {}, []);
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { logout } = useAuth();
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      try {
+        const res = await authFetch(GET_USER_CREATED_RECIPES_URL, {}, logout);
+        const data = await res.json();
+        setRecipes(data);
+        setIsLoading(false);
+
+        // Catch 401 Unauthorized
+      } catch (error) {
+        console.log("Request failed:", error.message);
+        toast.error("Unauthorized");
+      }
+    }
+    fetchRecipes();
+  }, []);
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const recipesPerPage = 12;
+  const totalPages = Math.ceil(recipes.length / recipesPerPage);
+  const paginatedRecipes = recipes.slice(
+    (currentPage - 1) * recipesPerPage,
+    currentPage * recipesPerPage
+  );
 
   return (
     <div>
@@ -14,7 +47,13 @@ function UserRecipes() {
       <Link to="/CreateRecipe" className="btn mt-5 mb-5 btn-primary">
         Create Recipe
       </Link>
-      <RecipeContainer recipes={[]} />
+      <RecipeContainer
+        recipes={paginatedRecipes}
+        isLoading={isLoading}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
