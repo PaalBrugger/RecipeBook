13a54253cbbce99d3097f064e8d0204cc06cec74
paalrebnordbrugger <paalbrugger@gmail.com>
2026-03-04 15:06:58 +0100

    Added auth URL´S

--- ClientApp/src/utils/apiUrls.js
+++ ClientApp/src/utils/apiUrls.js
@@ -2,6 +2,11 @@
 export const BASE_URL = "http://localhost:5091";
 export const API_BASE_URL = "http://localhost:5091/api";
 
+// Auth
+export const REGISTER_USER_URL = `${API_BASE_URL}/auth/register`;
+export const CHECK_USERNAME_URL = `${API_BASE_URL}/auth/check-username`;
+export const LOGIN_URL = `${API_BASE_URL}/auth/login`;
+
 // Recipe
 export const FILTER_URL = `${API_BASE_URL}/recipe/filter`;
 export const RANDOM_URL = `${API_BASE_URL}/recipe/random`;
@@ -9,14 +14,11 @@ export const SEARCH_URL = `${API_BASE_URL}/recipe/search`;
 export const LOOKUP_ID_URL = `${API_BASE_URL}/recipe/`;
 
 // User
-
+export const USER_URL = `${API_BASE_URL}/user/me`;
 export const FAVORITE_RECIPE_URL = `${API_BASE_URL}/user/favorite-recipe`;
 export const UNFAVORITE_RECIPE_URL = `${API_BASE_URL}/user/unfavorite-recipe`;
 export const ISFAVORITED_RECIPE_URL = `${API_BASE_URL}/user/is-favorited`;
 export const GET_FAVORITED_RECIPES_URL = `${API_BASE_URL}/user/get-favorited-recipes`;
-
-export const USER_URL = `${API_BASE_URL}/user/me`;
-
 export const CREATE_RECIPE_URL = `${API_BASE_URL}/user/create-recipe`;
 export const GET_USER_CREATED_RECIPES_URL = `${API_BASE_URL}/user/get-user-created-recipes`;
 export const UPDATE_RECIPE_URL = `${API_BASE_URL}/user/update-recipe`;
