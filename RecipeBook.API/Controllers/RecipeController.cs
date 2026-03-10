6adca01fa0ce88de109318aaf7f764b344a6227b
paalrebnordbrugger <paalbrugger@gmail.com>
2026-03-10 14:47:30 +0100

    Removed imports

--- RecipeBook.API/Controllers/RecipeController.cs
+++ RecipeBook.API/Controllers/RecipeController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RecipeBook.API.Database;
